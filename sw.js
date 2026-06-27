3bbb85fcf840919d24aa12885c38bbff650e9a16
francesco <francesco.motta53@gmail.com>
2026-06-27 20:05:53 +0200

    Deploy app-v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'apiario-v15'
+const CACHE = 'apiario-v16'
 const STATIC = ['./manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png']
 
 self.addEventListener('install', e => {
