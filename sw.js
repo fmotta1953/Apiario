8b2912f5e9e269292c55812bb360b76cbcb244ab
francesco <francesco.motta53@gmail.com>
2026-06-27 19:32:57 +0200

    Deploy v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'apiario-v1'
+const CACHE = 'apiario-v3'
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png']
 
 self.addEventListener('install', e => {
@@ -12,5 +12,14 @@ self.addEventListener('activate', e => {
   self.clients.claim()
 })
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+  // Network first: scarica sempre la versione aggiornata, fallback alla cache se offline
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, copy))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
 })
